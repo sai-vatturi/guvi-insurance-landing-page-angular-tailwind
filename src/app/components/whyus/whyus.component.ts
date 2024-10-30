23a91f40d9f37e78589fc00ebe0a050179d7985d
Sainadh V <sainadhvatturi@gmail.com>
2024-10-31 02:00:25 +0530

    whyus component complete

--- src/app/components/whyus/whyus.component.ts
+++ src/app/components/whyus/whyus.component.ts
@@ -1,12 +1,39 @@
+// whyus.component.ts
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-whyus',
-  standalone: true,
-  imports: [],
-  templateUrl: './whyus.component.html',
-  styleUrl: './whyus.component.css'
+	selector: 'app-whyus',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './whyus.component.html',
+	styleUrl: './whyus.component.css'
 })
 export class WhyusComponent {
-
+	features = [
+		{
+			title: 'Protective Protection',
+			icon: '✓'
+		},
+		{
+			title: 'Money Back Guarantee',
+			icon: '✓'
+		},
+		{
+			title: 'Homeowner Insurance',
+			icon: '✓'
+		},
+		{
+			title: 'Partnership System',
+			icon: '✓'
+		},
+		{
+			title: 'Consultations',
+			icon: '✓'
+		},
+		{
+			title: 'Safety Ensure',
+			icon: '✓'
+		}
+	];
 }
