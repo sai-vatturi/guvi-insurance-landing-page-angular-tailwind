446cea8657c8dba97f88bea492170871910c27cb
Sainadh V <sainadhvatturi@gmail.com>
2024-10-31 00:42:45 +0530

    complete features component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogandnewsComponent } from './components/blogandnews/blogandnews.component';
+import { ExpertComponent } from './components/expert/expert.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { FeaturesComponent } from './components/features/features.component';
-import { AboutComponent } from './components/about/about.component';
 import { InsuranceComponent } from './components/insurance/insurance.component';
-import { WhyusComponent } from './components/whyus/whyus.component';
-import { ExpertComponent } from './components/expert/expert.component';
-import { ReviewComponent } from './components/review/review.component';
-import { BlogandnewsComponent } from './components/blogandnews/blogandnews.component';
 import { ReadyComponent } from './components/ready/ready.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ReviewComponent } from './components/review/review.component';
+import { WhyusComponent } from './components/whyus/whyus.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    HeroComponent,
-    FeaturesComponent,
-    AboutComponent,
-    InsuranceComponent,
-    WhyusComponent,
-    ExpertComponent,
-    ReviewComponent,
-    BlogandnewsComponent,
-    ReadyComponent,
-    FooterComponent,
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent, HeroComponent, FeaturesComponent, AboutComponent, InsuranceComponent, WhyusComponent, ExpertComponent, ReviewComponent, BlogandnewsComponent, ReadyComponent, FooterComponent],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'insurance-website';
+	title = 'insurance-website';
 }
--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-features',
-  standalone: true,
-  imports: [],
-  templateUrl: './features.component.html',
-  styleUrl: './features.component.css'
+	selector: 'app-features',
+	standalone: true,
+	imports: [],
+	templateUrl: './features.component.html',
+	styleUrl: './features.component.css'
 })
-export class FeaturesComponent {
-
-}
+export class FeaturesComponent {}
